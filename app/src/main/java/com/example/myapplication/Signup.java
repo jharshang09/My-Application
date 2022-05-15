8ebb8998e95f30672bdfd6693b5d2bbc2479374d
Harshang Jadav <jharshang909@gmail.com>
2022-05-15 16:51:44 +0530

    commit

--- app/src/main/java/com/example/myapplication/Signup.java
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -11,8 +11,7 @@ public class Signup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up);
+        setContentView(R.layout.activity_homescreen);
 
     }
-
 }
